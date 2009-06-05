b54f4c386851cc32404caa694a4834c8f3937b5e
James Lynch <lynchjames@gmail.com>
2009-06-05 08:16:23 +0000

    Batch file to create .mx file from MGrammar

---
+++ Core/Grammar/bdUnitMGrammar.bat
@@ -0,0 +1,5 @@
+rem This batch file can be used to compile the Simple.mg file into Simple.mx, which
+rem can then be loaded by DynamicParser.
+@echo off
+"C:\Program Files\Microsoft Oslo SDK 1.0\Bin\m.exe" bdUnitMGrammar.mg
+pause
